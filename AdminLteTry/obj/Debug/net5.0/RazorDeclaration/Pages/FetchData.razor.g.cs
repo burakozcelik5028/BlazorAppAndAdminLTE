38cf8fcb69bd518b846b47b615623d905e97d838
burak.ozcelik <b.ozcelik@baylanwatermeters.com>
2021-06-11 09:56:30 +0300

    ReBuild Project and Nuget package
    fix DirectoryNotFoundException

--- AdminLteTry/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ AdminLteTry/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,84 +13,84 @@ namespace AdminLteTry.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\BurakOzcelik\source\repos\AdminLteTry\AdminLteTry\_Imports.razor"
+#line 1 "C:\Users\BurakOzcelik\source\repos\BlazorAppAndAdminLTE\AdminLteTry\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\BurakOzcelik\source\repos\AdminLteTry\AdminLteTry\_Imports.razor"
+#line 2 "C:\Users\BurakOzcelik\source\repos\BlazorAppAndAdminLTE\AdminLteTry\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\BurakOzcelik\source\repos\AdminLteTry\AdminLteTry\_Imports.razor"
+#line 3 "C:\Users\BurakOzcelik\source\repos\BlazorAppAndAdminLTE\AdminLteTry\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\BurakOzcelik\source\repos\AdminLteTry\AdminLteTry\_Imports.razor"
+#line 4 "C:\Users\BurakOzcelik\source\repos\BlazorAppAndAdminLTE\AdminLteTry\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\BurakOzcelik\source\repos\AdminLteTry\AdminLteTry\_Imports.razor"
+#line 5 "C:\Users\BurakOzcelik\source\repos\BlazorAppAndAdminLTE\AdminLteTry\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\BurakOzcelik\source\repos\AdminLteTry\AdminLteTry\_Imports.razor"
+#line 6 "C:\Users\BurakOzcelik\source\repos\BlazorAppAndAdminLTE\AdminLteTry\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\BurakOzcelik\source\repos\AdminLteTry\AdminLteTry\_Imports.razor"
+#line 7 "C:\Users\BurakOzcelik\source\repos\BlazorAppAndAdminLTE\AdminLteTry\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\BurakOzcelik\source\repos\AdminLteTry\AdminLteTry\_Imports.razor"
+#line 8 "C:\Users\BurakOzcelik\source\repos\BlazorAppAndAdminLTE\AdminLteTry\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\BurakOzcelik\source\repos\AdminLteTry\AdminLteTry\_Imports.razor"
+#line 9 "C:\Users\BurakOzcelik\source\repos\BlazorAppAndAdminLTE\AdminLteTry\_Imports.razor"
 using AdminLteTry;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\BurakOzcelik\source\repos\AdminLteTry\AdminLteTry\_Imports.razor"
+#line 10 "C:\Users\BurakOzcelik\source\repos\BlazorAppAndAdminLTE\AdminLteTry\_Imports.razor"
 using AdminLteTry.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\BurakOzcelik\source\repos\AdminLteTry\AdminLteTry\_Imports.razor"
+#line 12 "C:\Users\BurakOzcelik\source\repos\BlazorAppAndAdminLTE\AdminLteTry\_Imports.razor"
 using Blazor.AdminLte;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\BurakOzcelik\source\repos\AdminLteTry\AdminLteTry\Pages\FetchData.razor"
+#line 3 "C:\Users\BurakOzcelik\source\repos\BlazorAppAndAdminLTE\AdminLteTry\Pages\FetchData.razor"
 using AdminLteTry.Data;
 
 #line default
@@ -105,7 +105,7 @@ using AdminLteTry.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 39 "C:\Users\BurakOzcelik\source\repos\AdminLteTry\AdminLteTry\Pages\FetchData.razor"
+#line 39 "C:\Users\BurakOzcelik\source\repos\BlazorAppAndAdminLTE\AdminLteTry\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
